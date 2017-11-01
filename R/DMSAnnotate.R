16b7524cb26300999a50d85f894511950792e889
ritianjiang <1haotian@live.cn>
2017-11-01 11:40:02 +0800

    add method for GRanges/repair the document page.

--- R/DMSAnnotate.R
+++ R/DMSAnnotate.R
@@ -1,12 +1,12 @@
 #'The function annotates the methylDiff/methylBase obj.
 #'
-#' @param object: A methylDiff/methyBase object
+#' @param object: A methylDiff/methyBase/GRanges object
 #' @param grAnno: A GRanges object
 #' @export
 #' @return  A dataframe which contains site location and annotation. In the dataframe, there won't be the metainfo like p-values etc.
 #' @examples
 #' data(ExampleAnnotate)
-#' result<-DMSAnnotate()
+#' result<-DMSAnnotate(methylDiff.obj, ExampleGencode)
 #'
 #'
 #'
@@ -14,21 +14,32 @@
 setGeneric("DMSAnnotate",function(object,grAnno){standardGeneric("DMSAnnotate")})
 
 setMethod("DMSAnnotate","methylDiff",
-           function(object,grAnno){
-                 data<-getData(object);
-                 dataGR<-with(data,GRanges(chr,IRanges(start,end),strand=strand))
-                 data.anno<-findOverlaps(dataGR,grAnno)
-                 annoGrange<-data.frame(grAnno[data.anno@to])
-                 Result<-cbind(dataGR[data.anno@from],annoGrange)
-                 return(Result)
-           })
+          function(object,grAnno){
+            data<-getData(object);
+            dataGR<-with(data,GRanges(chr,IRanges(start,end),strand=strand,diff = meth.diff))
+            data.anno<-findOverlaps(dataGR,grAnno)
+            annoGrange<-data.frame(grAnno[data.anno@to])
+            Result<-cbind(dataGR[data.anno@from],annoGrange,stringsAsFactors = F)
+            gc()
+            return(Result)
+          })
 
 setMethod("DMSAnnotate","methylBase",
           function(object,grAnno){
             data<-getData(object);
-            dataGR<-with(data,GRanges(chr,IRanges(start,end),strand=strand))
+            dataGR<-with(data,GRanges(chr,IRanges(start,end),strand=strand,diff = meth.diff))
             data.anno<-findOverlaps(dataGR,grAnno)
             annoGrange<-data.frame(grAnno[data.anno@to])
-            Result<-cbind(dataGR[data.anno@from],annoGrange)
+            Result<-cbind(dataGR[data.anno@from],annoGrange,stringsAsFactors = F)
+            gc();
+            return(Result)
+          })
+
+setMethod("DMSAnnotate","GRanges",
+          function(object,grAnno){
+            data.anno<-findOverlaps(object,grAnno)
+            annoGrange<-data.frame(grAnno[data.anno@to])
+            Result<-cbind(object[data.anno@from],annoGrange,stringsAsFactors = F)
+            gc();
             return(Result)
           })
