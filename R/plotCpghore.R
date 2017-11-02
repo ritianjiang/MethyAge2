7db077239abe231e7da4491bd37c334ecf313ca6
ritianjiang <1haotian@live.cn>
2017-11-02 12:26:37 +0800

    Repair vital errors!!

--- R/plotCpghore.R
+++ R/plotCpghore.R
@@ -26,23 +26,26 @@ setMethod("plotCpghore","GRanges",
             up<-MethAnno[MethAnno$ann == "up200",]
             cpg<-MethAnno[MethAnno$ann != "up200" & MethAnno$ann != "down200",]
 
-            down$dist<-down[,ncol(down)-3] - down$start
-            down_mean<-aggregate(down[,6:(ncol(down)-7)],by = list(down$dist),FUN = mean)
-            down_mean$Group.1<-down_mean$Group.1 + len
+            down$dist<- abs(down[,ncol(down)-4] - down$start)
+            down_mean<- aggregate(down[,6:(ncol(down)-7)],by = list(down$dist),FUN = mean)
+            down_mean$Group.1<- down_mean$Group.1 + len
 
-            cpg$dist<-((cpg[,ncol(cpg)-3] - cpg$start)/(cpg[,ncol(cpg)-2]))*len
+            cpg$dist<-((cpg$start-cpg[,ncol(cpg)-4])/(cpg[,ncol(cpg)-2]))*len
             cpg$dist<-as.integer(cpg$dist)
             cpg_mean<-aggregate(cpg[,6:(ncol(cpg)-7)],by = list(cpg$dist),FUN = mean)
 
-            up$dist<-up[,ncol(up)-4] - up$start
+            up$dist<-up$start-up[,ncol(up)-3]
             up_mean<-aggregate(up[,6:(ncol(up)-7)],by = list(up$dist),FUN = mean)
 
+            up_mean$Group.1<-as.character(up_mean$Group.1)
+            cpg_mean$Group.1<-as.character(cpg_mean$Group.1)
+            down_mean$Group.1<-as.character(down_mean$Group.1)
 
             totalcpg<-rbind(up_mean,cpg_mean)
             totalcpg<-rbind(totalcpg,down_mean)
             totalcpg<-aggregate(totalcpg[,-1],by = list(totalcpg$Group.1),FUN = mean)
-            totalcpg_app<-data.frame(loc = totalcpg$Group.1
-                                     ,meth = apply(totalcpg,MARGIN = 1,FUN = mean))
+            totalcpg_app<-data.frame(loc = as.numeric(totalcpg$Group.1)
+                                     ,meth = apply(totalcpg[,-1],MARGIN = 1,FUN = mean))
             if(plot == T){
               silda<-filter((totalcpg_app[,2])/win_len,rep(1,win_len))
               silda<-data.frame(loc = totalcpg_app$loc,
